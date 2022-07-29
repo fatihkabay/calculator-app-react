dd116eecc21871076e9907dedb5cf45b160ef766
Fatih Kabay <mfthkabay@gmail.com>
2022-07-29 15:55:46 +0300

    edit css

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+        <span>Calc-u-later</span>
+        <div className='calculator'></div>
+    </div>
+  );
+}
+
+export default App;
