2777dd851771dcf7869b9cb916aff02c7d69682b
Fatih Kabay <mfthkabay@gmail.com>
2022-07-30 14:44:45 +0300

    new component create

--- src/App.jsx
+++ src/App.jsx
@@ -8,33 +8,7 @@ function App() {
         <div className="calc-wrapper">
           <input className="calc-input" type="text" />
           <hr />
-          <div className="buttons">
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            <button className="calc-button">wf</button>
-            
-          </div>
+         
         </div>
       </div>
     </div>
